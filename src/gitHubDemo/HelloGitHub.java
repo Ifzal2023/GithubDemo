efe32b791e7cf0698ac404a89ab9d32474d939ca
Ifzal2023 <ifzal.mi@gmail.com>
2023-01-30 13:00:57 -0600

    3rd push

--- src/gitHubDemo/HelloGitHub.java
+++ src/gitHubDemo/HelloGitHub.java
@@ -1,9 +1,10 @@
 package gitHubDemo;
 
-//Creation of Separate github Repository
+//Creation of Separate github Repositorygit
 
 public class HelloGitHub {
 	public static void main(String[] args) {
-		System.out.println("I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
+		System.out.println(
+				"I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
 	}
 }
